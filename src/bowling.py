5609c4378229831edcb41b7c1d88ebf9cbb7f4e7
neoprincie <bryprinc@gmail.com>
2024-11-14 23:07:46 -0500

    spare

--- src/bowling.py
+++ src/bowling.py
@@ -1,10 +1,22 @@
 class Bowling:
     def __init__(self):
         self._currentScore = 0
+        self.rolls = []
 
     def roll(self, pins):
         self._currentScore += pins
+        self.rolls.append(pins)
         pass
 
     def score(self):
-        return self._currentScore
+        total_score = 0
+        roll_index = 0
+        for i in range(10):
+            if self.rolls[roll_index] + self.rolls[roll_index + 1] == 10:
+                total_score += 10 + self.rolls[roll_index + 2]
+            else:
+                total_score += self.rolls[roll_index] + self.rolls[roll_index + 1]
+
+            roll_index += 2
+
+        return total_score
--- tests/bowling_tests.py
+++ tests/bowling_tests.py
@@ -4,21 +4,30 @@ from src import bowling
 
 
 class TestBowling(unittest.TestCase):
+    def setUp(self):
+        self.game = bowling.Bowling()
+
     def test_all_gutters(self):
-        game = bowling.Bowling()
-
         for i in range(20):
-            game.roll(0)
+            self.game.roll(0)
 
-        self.assertEqual(game.score(), 0)  # add assertion here
+        self.assertEqual(self.game.score(), 0)  # add assertion here
 
     def test_all_ones(self):
-        game = bowling.Bowling()
-
         for i in range(20):
-            game.roll(1)
+            self.game.roll(1)
 
-        self.assertEqual(game.score(), 20)
+        self.assertEqual(self.game.score(), 20)
+
+    def test_spares(self):
+        self.game.roll(3)
+        self.game.roll(7)
+        self.game.roll(4)
+
+        for i in range(17):
+            self.game.roll(0)
+
+        self.assertEqual(18, self.game.score())
 
 
 if __name__ == '__main__':
